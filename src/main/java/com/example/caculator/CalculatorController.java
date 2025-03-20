0fc9f267e591b122cc6b6e8dd2a2c47fd66b6901
junyi0115 <eunjunyi@gmail.com>
2025-03-20 21:51:03 +0900

    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

--- src/main/java/com/example/caculator/CalculatorController.java
+++ src/main/java/com/example/caculator/CalculatorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CalculatorController { 
+public class CalculatorController {  
 
 	@Autowired
 	private CalculatorService service;
