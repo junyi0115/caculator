966387d6736e729c0203090229b548f498b19cb8
junyi0115 <eunjunyi@gmail.com>
2025-03-20 22:40:58 +0900

    vsdf

--- src/test/java/com/example/caculator/CaculatorApplicationTests.java
+++ src/test/java/com/example/caculator/CaculatorApplicationTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 //@SpringBootTest
-class CaculatorApplicationTests {
+class CaculatorApplicationTests { 
 
 	private CalculatorService service = new CalculatorService();
 	
